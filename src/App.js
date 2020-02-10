694e61eef6081e0531b35a2c217666d689387934
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-02-10 16:07:40 -0300

    Iniciando a aplicação front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './views/login'
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  /* COMENTÁRIO JSX
+  state = {
+    numero1: null,
+    numero2: null,
+    resultado: null,
+  }
+
+  somar = () => {
+    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
+    this.setState({resultado: resultado});
+  } */
+
+  render(){
+    /*return(
+      <div>
+        <label>Primeiro número:</label>
+        <input type="text" value={this.state.numero1} 
+                onChange={(e) => this.setState({numero1: e.target.value}) } />
+        <br />
+
+        <label>Segundo número:</label>
+        <input type="text" value={this.state.numero2} 
+                onChange={(e) => this.setState({numero2: e.target.value}) } />
+        <br />
+
+        <button onClick = { this.somar } >
+          Somar
+        </button>
+        <br />
+
+        O nome digitado foi: {this.state.resultado}
+      </div>
+    )*/
+    return(
+      <div>
+        <Login />
+      </div>
+    )
+  }
 }
 
 export default App;
