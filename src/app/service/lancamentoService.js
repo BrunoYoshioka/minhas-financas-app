0d78d4fa81e1f34317683051e6abf894923b98ba
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-02-17 12:21:18 -0300

    Consulta e Exclusão de Lançamentos finalizado

--- src/app/service/lancamentoService.js
+++ src/app/service/lancamentoService.js
@@ -1,9 +1,35 @@
 import ApiService from '../apiservice'
 
-export default class LancamentoService extends ApiService{
-    
+export default class LancamentoService extends ApiService {
+
     constructor(){
         super('/api/lancamentos')
+    }
+
+    obterListaMeses(){
+        return [
+            { label: 'Selecione...', value: '' },
+            { label: 'Janeiro', value: 1 },
+            { label: 'Fevereiro', value: 2 },
+            { label: 'Março', value: 3 },
+            { label: 'Abril', value: 4 },
+            { label: 'Maio', value: 5 },
+            { label: 'Junho', value: 6 },
+            { label: 'Julho', value: 7 },
+            { label: 'Agosto', value: 8 },
+            { label: 'Setembro', value: 9 },
+            { label: 'Outubro', value: 10 },
+            { label: 'Novembro', value: 11 },
+            { label: 'Dezembro', value: 12 },
+        ]
+    }
+
+    obterListaTipos(){
+        return [
+            { label: 'Selecione...', value: '' },
+            { label: 'Despesa' , value : 'DESPESA' },
+            { label: 'Receita' , value : 'RECEITA' }
+        ]
     }
 
     consultar(lancamentoFiltro){
@@ -27,6 +53,14 @@ export default class LancamentoService extends ApiService{
             params = `${params}&usuario=${lancamentoFiltro.usuario}`
         }
 
+        if(lancamentoFiltro.descricao){
+            params = `${params}&descricao=${lancamentoFiltro.descricao}`
+        }
+
         return this.get(params);
     }
-}
+
+    deletar(id){
+        return this.delete(`/${id}`)
+    }
+} 
--- src/views/lancamentos/consulta-lancamentos.js
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -1,13 +1,14 @@
-
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-
 import Card from '../../components/card'
 import FormGroup from '../../components/form-group'
 import SelectMenu from '../../components/selectMenu'
 import LancamentosTable from './lancamentosTable'
+
 import LancamentoService from '../../app/service/lancamentoService'
-import LocalStorageService from '../../app/service/lancamentoService'
+import LocalStorageService from '../../app/service/localstorageService'
+
+import * as messages from '../../components/toastr' // importar tudo que esta dentro do toastr com o nome messages
 
 class ConsultaLancamentos extends React.Component {
 
@@ -15,6 +16,7 @@ class ConsultaLancamentos extends React.Component {
         ano: '',
         mes: '',
         tipo: '',
+        descricao: '',
         lancamentos : []
     }
 
@@ -24,12 +26,18 @@ class ConsultaLancamentos extends React.Component {
     }
 
     buscar = () => {
+        if(!this.state.ano){ // se não passou o ano
+            messages.mensagemErro('O preenchimento do campo Ano é obrigatório.')
+            return false;
+        }
+
         const usuarioLogado = LocalStorageService.obterItem('_usuario_logado');
 
         const lancamentoFiltro = {
             ano: this.state.ano,
             mes: this.state.mes,
             tipo: this.state.tipo,
+            descricao: this.state.descricao,
             usuario: usuarioLogado.id
         }
 
@@ -40,31 +48,32 @@ class ConsultaLancamentos extends React.Component {
             }).catch( error => {
                 console.log(error)
             })
-            //console.log(this.state)
+    }
+
+    editar = (id) => {
+        console.log('Editando o lancamento', id)
+    }
+
+    deletar = ( lancamento ) => {
+        // console.log('Deletando o lancamento', id)
+        this.service
+            .deletar(lancamento.id)
+            .then(response => {
+                // remover do array
+                const lancamentos = this.state.lancamentos; // capturar lancamentos na constante
+                const index = lancamentos.indexOf(lancamento) // descobrir qual item do lancamento que eu quero deletar
+                lancamentos.splice(index, 1); // Esse método recebe os parametros 
+                this.setState(lancamentos) // atualizar a variável lançamentos
+
+                messages.mensagemSucesso('Lançamento excluído com sucesso!')
+            }).catch(error => {
+                messages.mensagemErro('Ocorreu um erro ao tentar deletar o Lançamento')
+            })
     }
 
     render(){
-        const meses = [
-            { label: 'Selecione...', value: '' },
-            { label: 'Janeiro', value: 1 },
-            { label: 'Fevereiro', value: 2 },
-            { label: 'Março', value: 3 },
-            { label: 'Abril', value: 4 },
-            { label: 'Maio', value: 5 },
-            { label: 'Junho', value: 6 },
-            { label: 'Julho', value: 7 },
-            { label: 'Agosto', value: 8 },
-            { label: 'Setembro', value: 9 },
-            { label: 'Outubro', value: 10 },
-            { label: 'Novembro', value: 11 },
-            { label: 'Dezembro', value: 12 },
-        ]
-
-        const tipos = [
-            { label: 'Selecione...', value: '' },
-            { label: 'Despesa' , value : 'DESPESA' },
-            { label: 'Receita' , value : 'RECEITA' }
-        ]
+        const meses = this.service.obterListaMeses();
+        const tipos = this.service.obterListaTipos();
 
         return (
             <Card title="Consulta Lançamentos">
@@ -74,7 +83,7 @@ class ConsultaLancamentos extends React.Component {
                             <FormGroup htmlFor="inputAno" label="Ano: *">
                                 <input type="text" 
                                        className="form-control" 
-                                       id="inputAno"
+                                       id="inputAno" 
                                        value={this.state.ano}
                                        onChange={e => this.setState({ano: e.target.value})}
                                        placeholder="Digite o Ano" />
@@ -86,6 +95,15 @@ class ConsultaLancamentos extends React.Component {
                                             onChange={e => this.setState({ mes: e.target.value })}
                                             className="form-control" 
                                             lista={meses} />
+                            </FormGroup>
+
+                            <FormGroup htmlFor="inputDesc" label="Descricao: ">
+                                <input type="text" 
+                                       className="form-control" 
+                                       id="inputDescricao" 
+                                       value={this.state.descricao}
+                                       onChange={e => this.setState({descricao: e.target.value})}
+                                       placeholder="Digite a Descricao" />
                             </FormGroup>
 
                             <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: ">
@@ -103,11 +121,13 @@ class ConsultaLancamentos extends React.Component {
 
                     </div>
                 </div>   
-                <br />
+                <br/>
                 <div className="row">
                     <div className="col-md-12">
                         <div className="bs-component">
-                            <LancamentosTable lancamentos={this.state.lancamentos} />
+                            <LancamentosTable lancamentos={this.state.lancamentos} 
+                                              deleteAction={this.deletar}
+                                              editAction={this.editar} />
                         </div>
                     </div>  
                 </div>            
@@ -116,4 +136,4 @@ class ConsultaLancamentos extends React.Component {
     }
 }
 
-export default withRouter(ConsultaLancamentos);
+export default withRouter(ConsultaLancamentos); 
