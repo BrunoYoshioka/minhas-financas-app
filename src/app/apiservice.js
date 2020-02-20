ad7a48ef2fcab3f9a7aed42f5c30220150e86566
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-02-20 15:44:54 -0300

    Adicionando a url da api remota

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://minhasfinancas-api-java.herokuapp.com'
 })
 
 class ApiService {
