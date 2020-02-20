9fd65829074d7863065b4bb38edb7ece4976339d
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-02-20 16:15:54 -0300

    Removendo a importação desnecessária do login.js

--- src/views/login.js
+++ src/views/login.js
@@ -4,7 +4,6 @@ import FormGroup from '../components/form-group'
 import { withRouter } from 'react-router-dom'
 
 import UsuarioService from '../app/service/usuarioService'
-import LocalStorageService from '../app/service/localstorageService'
 import { mensagemErro } from '../components/toastr'
 import { AuthContext } from '../main/provedorAutenticacao'
 
